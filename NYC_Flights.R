2b8a2d5534016dc6dc4da2073619b79a1b601485
Lukasz Suslowicz <susel.invest@gmail.com>
2023-11-10 16:39:33 +0100

    flights delay measure

--- NYC_Flights.R
+++ NYC_Flights.R
@@ -1,2 +1,18 @@
 library(nycflights13)
 library(tidyverse)
+
+# Badamy zależność między odległością a średnim opóźnieniem
+delays <- flights %>% 
+  group_by(dest) %>%
+  summarize(
+    count = n(),
+    dist = mean(distance, na.rm = TRUE),
+    delay = mean(arr_delay, na.rm = TRUE)
+  ) %>%
+  filter(count > 20, dest != "HNL") # Honolulu za daleko
+
+ggplot(data = delays, mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
+
+
